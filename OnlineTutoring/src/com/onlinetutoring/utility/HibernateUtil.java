a286ad28aa45cd55ddf9ae4c96aa5c713fd453af
ssnijik <87cccc@gmail.com>
2013-04-30 20:10:23 +0800

    The first attempt to submit Files: HibernateUtil.java Library: hibernate.core 		 mysql.connector 		 servlet.api

---
+++ OnlineTutoring/src/com/onlinetutoring/utility/HibernateUtil.java
@@ -0,0 +1,52 @@
+package com.onlinetutoring.utility;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * @author Ssn
+ * Hibernate Utility Class
+ */
+
+public class HibernateUtil {
+	private static Configuration cfg;
+	private static ServiceRegistry  sr;
+	private static SessionFactory sessionFactory;
+	
+	static {
+		try {
+			//create new SessionFactory from hibernate.cfg.xml
+			cfg = new Configuration().configure("/hibernate.cfg.xml");
+			sr = new ServiceRegistryBuilder().applySettings(cfg.getProperties()).buildServiceRegistry();
+			sessionFactory = cfg.buildSessionFactory(sr);
+		}
+		catch (Throwable ex) {
+			// If something goes wrong, it will throw an exception
+			System.err.println("Initial SessionFactory creation failed. " + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+	/**
+	 * Open a new session
+	 * @return Session
+	 */
+	
+	public static Session openSession() {
+		return sessionFactory.openSession();
+	}
+	
+	/**
+	 * Close the current session
+	 * @param session
+	 */
+	
+	public static void closeSession(Session session) {
+		if (session != null) {
+			session.close();
+		}
+	}
+}
